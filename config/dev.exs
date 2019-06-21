f28e0d480c7f755eb9f4a624168d799645bc14e5
Filip Pavic <pavicfp@gmail.com>
2019-06-21 14:02:48 +0200

    debug_errors true in dev

--- config/dev.exs
+++ config/dev.exs
@@ -17,7 +17,7 @@ config :apis, Apis.Repo,
 # with webpack to recompile .js and .css sources.
 config :apis, ApisWeb.Endpoint,
   http: [port: 4000],
-  debug_errors: true,
+  debug_errors: false,
   code_reloader: true,
   check_origin: false,
   watchers: []
